eb0ac8be504b0aba187ad87fdd53fd3cd03cbd17
lei liu <liuleiwangyue@gmail.com>
2020-01-29 17:00:58 +1100

    import reactive form module

--- Angular7/src/app/app.module.ts
+++ Angular7/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
@@ -14,7 +14,8 @@ import { RegistrationComponent } from './user/registration/registration.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
